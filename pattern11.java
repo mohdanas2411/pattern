ef6b4ca0defcebed648745f3e5f1af15c8cf2a17
mohdanas2411 <anas06132@gmail.com>
2022-02-05 19:06:12 +0530

    commit sec

--- pattern11.java
+++ pattern11.java
@@ -7,7 +7,7 @@ public class pattern11 {
         int n = scn.nextInt();
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
-                if((i+j) == n/2 || (j-i) == n/2 || (i-j) == n/2 || (i+j) == (n-1+(n/2))) System.out.print("*\t");
+                 System.out.print("*\t");
                 else System.out.print("\t");
             }   
             System.out.println();
