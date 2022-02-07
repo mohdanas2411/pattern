60c876699c16bdaad65b18bca6aa3f4f7eb20d3c
mohdanas2411 <anas06132@gmail.com>
2022-02-07 23:02:31 +0530

    Pattern 19

---
+++ pattern19.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class pattern19{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            if(i==0){
+                if(j<=(n/2) || j==(n-1)) 
+                    System.out.print("*\t");
+                else System.out.print("\t");
+            }
+                
+            if(i>0 && i<(n/2)){
+                if(j==(n/2) || j==(n-1))
+                    System.out.print("*\t");
+                else System.out.print("\t");
+            }
+            if(i==(n/2)){
+                System.out.print("*\t");
+            }
+            if(i<n-1 && i>(n/2)){
+                if(j==(n/2) || j==0)
+                    System.out.print("*\t");
+                else System.out.print("\t");
+            }
+            if(i==n-1){
+                if(j>=(n/2) || j==0) 
+                    System.out.print("*\t");
+                else System.out.print("\t");
+            }
+            
+        }
+        System.out.println();
+    }
+
+ }
+}
